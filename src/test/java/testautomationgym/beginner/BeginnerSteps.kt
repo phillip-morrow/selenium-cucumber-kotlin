b46dceaa4303ed823454e779b1c474e16c64bf42
phillip <phillip.l.morrow@gmail.com>
2020-08-27 00:07:58 -0500

    converted to kotlin, updated selenium to 4.0.0 alpha 6, cucumber is updated to cucumber 6

--- src/test/java/testautomationgym/beginner/BeginnerSteps.kt
+++ src/test/java/testautomationgym/beginner/BeginnerSteps.kt
@@ -2,17 +2,30 @@ package testautomationgym.beginner
 
 import io.cucumber.java8.En
 import org.openqa.selenium.WebDriver
-import io.cucumber.java8.HookBody
 import io.cucumber.java8.Scenario
-import org.openqa.selenium.firefox.FirefoxDriver
-import io.cucumber.java8.StepDefinitionBody
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.openqa.selenium.chrome.ChromeDriver
 
 class BeginnerSteps : En {
     var driver: WebDriver? = null
     val BASE_URL = "localhost:3000"
 
     init {
-        Before { scenario: Scenario? -> driver = FirefoxDriver() }
-        Given("^I am on the {string} page$") { page: String? -> driver!![BASE_URL] }
+        Before { scenario: Scenario? ->
+            run {
+                WebDriverManager.chromedriver().setup()
+                driver = ChromeDriver()
+            }
+        }
+
+        After { scenario: Scenario? ->
+            run {
+                if (scenario?.isFailed!!) driver!!.quit()
+            }
+        }
+
+        Given("I am on the {string} page") { page: String? ->
+            driver!![BASE_URL]
+        }
     }
 }
--- src/test/java/testautomationgym/beginner/BeginnerTest.kt
+++ src/test/java/testautomationgym/beginner/BeginnerTest.kt
@@ -5,5 +5,5 @@ import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
-@CucumberOptions(features = ["java/testautomationgym/beginner/gherkin"])
+@CucumberOptions(features = ["src/test/java/testautomationgym/beginner/gherkin"])
 class BeginnerTest 
